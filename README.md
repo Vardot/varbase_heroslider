9013e4dd666d2eb64c28ec9f6a4caa15aaceeaf0
Rajab Natshah <rajabn@gmail.com>
2024-02-12 12:41:24 +0300

    Issue #3388221: Initialize Varbase Heroslider with Bootstrap ~5.3.0 Carousals and Single Directory Components (SDC)

---
+++ README.md
@@ -0,0 +1,28 @@
+# Varbase Heroslider
+---
+
+This app will populate the universal implementation of a "Hero Slider" to be
+ used on the homepage in most cases.
+
+A rich hero slider that allows you to display video and/or image slides.
+
+Now you can install Varbase Heroslider with any Drupal 10 sites. Even
+ if installed with the Minimal or Standard profile.
+
+However, using it with Varbase distro and Vartheme or Bootstrap theme gives
+ you way much more cool stuff!
+
+
+### Features
+* A full-width responsive Varbase Heroslider.
+* Easy to customize slides to act as a promotional feature typically
+  displayed in the site's homepage.
+* Queued Herosliders.
+
+
+### Join Our Slack Team for Feedback and Support
+[![](https://www.drupal.org/files/varbase-slack-cta_0.png)](http://slack.varbase.vardot.com)
+Join now: http://slack.varbase.vardot.com/
+
+
+This module is sponsored and developed by Vardot.
---
+++ js/video.heroslider.youtube.js
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * Behaviors of Varbase heroslider for Youtube video scripts.
+ */
+
+(function ($, Drupal) {
+  Drupal.behaviors.varbaseHeroSlider_youtube = {
+    attach(context) {
+      const mediaSliders = $(
+        '.slick--view--varbase-heroslider .slick__slider',
+        context,
+      );
+      // On before slide change.
+      mediaSliders.on(
+        'beforeChange',
+        function (event, slick, currentSlide, nextSlide) {
+          const currentVideo = $(`.slide--${currentSlide}.slick-active`).find(
+            '.varbase-video-player iframe[src*="youtube.com"]',
+            context,
+          );
+          if (currentVideo.length > 0) {
+            currentVideo.get(0).contentWindow.postMessage('pause', '*');
+          }
+        },
+      );
+
+      // On after slide change.
+      mediaSliders.on('afterChange', function (event, slick, currentSlide) {
+        const currentVideo = $(`.slide--${currentSlide}.slick-active`).find(
+          '.varbase-video-player iframe[src*="youtube.com"]',
+          context,
+        );
+        if (currentVideo.length > 0) {
+          currentVideo.get(0).contentWindow.postMessage('play', '*');
+        } else {
+          mediaSliders.slick('slickPlay');
+        }
+      });
+
+      // On first slide load.
+      const firstIframeVideo = $('.varbase-heroslider')
+        .find('.slide')
+        .first()
+        .find('.varbase-video-player iframe[src*="youtube.com"]', context);
+      if (firstIframeVideo.length > 0) {
+        firstIframeVideo.on('load', function () {
+          mediaSliders.slick('slickPause');
+          $(this).get(0).contentWindow.postMessage('play', '*');
+        });
+      }
+
+      function youtubeActionProcessor(e) {
+        if (e.data === 'endedYoutube' || e.message === 'endedYoutube') {
+          if ($('.varbase-heroslider .slide').length > 1) {
+            // When having 2 or more slides.
+            mediaSliders.slick('slickNext');
+          } else {
+            // When only having one Youtube slide.
+            firstIframeVideo.get(0).contentWindow.postMessage('play', '*');
+          }
+        } else if (
+          e.data === 'playingYoutube' ||
+          e.message === 'playingYoutube'
+        ) {
+          mediaSliders.slick('slickPause');
+        }
+      }
+
+      // Setup the event listener for messaging.
+      if (window.addEventListener) {
+        window.addEventListener('message', youtubeActionProcessor, false);
+      } else {
+        window.attachEvent('onmessage', youtubeActionProcessor);
+      }
+    },
+  };
+})(window.jQuery, window.Drupal);
